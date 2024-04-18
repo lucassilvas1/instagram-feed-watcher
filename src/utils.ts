a1dd26e79a6727ace6c57de655be31031fffd05f
Lucas <lucass_silvas@hotmail.com>
2024-04-17 22:27:00 -0300

    make a reusable function to help find info in HTML

--- src/utils.ts
+++ src/utils.ts
@@ -5,3 +5,17 @@ export function random(min: number, max: number) {
 export function sleep(maxMs: number, minMs?: number): Promise<void> {
   return new Promise((res) => setTimeout(res, random(minMs ?? maxMs, maxMs)));
 }
+
+function escapeRegExp(text: string) {
+  return Array.from(text)
+    .map((char) => `\\u{${char.charCodeAt(0).toString(16)}}`)
+    .join("");
+}
+
+export function findBetween(text: string, prefix: string, suffix: string) {
+  const exp = new RegExp(
+    `${escapeRegExp(prefix)}(.*?)${escapeRegExp(suffix)}`,
+    "u"
+  );
+  return text.match(exp)?.[1];
+}
